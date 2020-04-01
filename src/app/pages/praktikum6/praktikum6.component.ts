2cf76d3ce7078f6cc7d26c68629bbfd0e0bdcc2e
fahrullyn <fahrulliansyah1800016127@webmail.uad.ac.id>
2020-04-01 22:38:31 +0700

    tekweb1800016127

---
+++ src/app/pages/praktikum6/praktikum6.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-praktikum6',
+  templateUrl: './praktikum6.component.html',
+  styleUrls: ['./praktikum6.component.scss']
+})
+export class Praktikum6Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
